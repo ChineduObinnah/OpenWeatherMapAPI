d6326d1d116cb3f6f89e7dd94d7e3cca56ab3760
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-06-17 11:42:17 -0500

    OpenWeather complete

--- OpenWeatherMapAPI/Program.cs
+++ OpenWeatherMapAPI/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+using Newtonsoft.Json.Linq;
+
+var apiKeyObj = File.ReadAllText("appsettings.json");
+
+var apiKey = JObject.Parse(apiKeyObj).GetValue("apiKey").ToString();
+
+var zip = 55420;
+
+var url = $"https://api.openweathermap.org/data/2.5/weather?zip={zip}&appid={apiKey}&units=imperial";
+
+var client  = new HttpClient();
+
+var response  = client.GetStringAsync(url).Result;
+
+var weatherObj = JObject.Parse(response);
+
+Console.WriteLine( $"Temp:{weatherObj["main"]["temp"]}");
